0d670ae18757be637fc225603bbe2490bd431e9e
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-10 23:25:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,15 +1,15 @@
 class Transfer
-  attr_accessor :sender, :receiver, :amount, :status
+  attr_accessor :sender, :receiver, :status, :amount
 
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
-    @amount = amount
     @status = "pending"
+    @amount = amount
   end
 
   def valid?
-    @sender.valid? && @receiver.valid? ? true : false
+    sender.valid? && receiver.valid? ? true : false
   end
 
   def execute_transaction
@@ -23,11 +23,4 @@ class Transfer
     end
   end
 
-  def reverse_transfer
-    if @status == "complete"
-      @sender.balance += @amount
-      @receiver.balance -= @amount
-      @status = "reversed"
-    end
-  end
 end
