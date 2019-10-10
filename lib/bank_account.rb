b30bcc1dd70971ef4d456848e4d6ac72f815124e
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-10 22:31:20 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,19 +1,15 @@
 class BankAccount
-  attr_accessor :status
+  attr_accessor :status, @balance
   attr_reader :name
 
   def initialize(name)
     @name = name
     @status = "open"
-    balance
-  end
-
-  def balance
-    p 1000
+    @balance = 1000
   end
 
   def deposit(amount)
-    balance += amount
+    @balance += amount
   end
 
 end
