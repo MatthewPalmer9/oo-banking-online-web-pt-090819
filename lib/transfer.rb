b76d952fe3ebe65f8c745d0a78aef657fbd82837
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-10 23:43:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.valid? && @status == "pending"
+    if @sender.balance >= @amount && @sender.valid? && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
